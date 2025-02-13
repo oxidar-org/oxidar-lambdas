dbcdf9e73dc7f718db68680cc859fb3b9c97b9b4
Nicolas Antinori <nicolas_antinori@hotmail.com>
2025-02-13 17:10:54 -0300

    abstracted away the tracing initialization

--- lambdas/validate_jwt/src/main.rs
+++ lambdas/validate_jwt/src/main.rs
@@ -2,17 +2,11 @@ mod error;
 mod generic_handler;
 mod models;
 
+use ::tracing::{initialize_tracing, otel_layer};
 use generic_handler::function_handler;
 use jsonwebtoken::jwk::JwkSet;
-use lambda_runtime::{
-    layers::{OpenTelemetryFaasTrigger, OpenTelemetryLayer as OtelLayer},
-    tower, tracing, Error, Runtime,
-};
-use opentelemetry::trace::TracerProvider;
-use opentelemetry_sdk::trace;
+use lambda_runtime::{tower, tracing, Error, Runtime};
 use tower::service_fn;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_subscriber::{prelude::*, EnvFilter};
 
 pub struct PersistedMemory {
     redis_client: redis::Client,
@@ -24,24 +18,7 @@ const APP_NAME: &str = "lambda_test";
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    // Set up OpenTelemetry tracer provider that writes spans to stdout for debugging purposes
-    // let exporter = opentelemetry_stdout::SpanExporter::default();
-    let formatting_layer = BunyanFormattingLayer::new(APP_NAME.into(), std::io::stdout);
-    let tracer_provider = trace::TracerProvider::builder()
-        //.with_batch_exporter(exporter, runtime::Tokio)
-        .build();
-
-    // Set up link between OpenTelemetry and tracing crate
-    tracing_subscriber::registry()
-        .with(tracing_opentelemetry::OpenTelemetryLayer::new(
-            tracer_provider.tracer(APP_NAME),
-        ))
-        .with(
-            EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(DEFAULT_LOG_LEVEL)),
-        )
-        .with(JsonStorageLayer)
-        .with(formatting_layer)
-        .init();
+    let tracing_provider = initialize_tracing(APP_NAME);
 
     tracing::info!("initializing lambda...");
 
@@ -62,15 +39,8 @@ async fn main() -> Result<(), Error> {
 
     // run(service_fn(|d| function_handler(d, &persisted))).await
     // Initialize the Lambda runtime and add OpenTelemetry tracing
-    let runtime = Runtime::new(service_fn(|d| function_handler(d, &persisted))).layer(
-        // Create a tracing span for each Lambda invocation
-        OtelLayer::new(|| {
-            // Make sure that the trace is exported before the Lambda runtime is frozen
-            tracer_provider.force_flush();
-        })
-        // Set the "faas.trigger" attribute of the span to "pubsub"
-        .with_trigger(OpenTelemetryFaasTrigger::Http),
-    );
+    let runtime = Runtime::new(service_fn(|d| function_handler(d, &persisted)))
+        .layer(otel_layer(&tracing_provider));
 
     runtime.run().await?;
     Ok(())
---
+++ tracing/src/lib.rs
@@ -0,0 +1,36 @@
+use lambda_runtime::layers::{OpenTelemetryFaasTrigger, OpenTelemetryLayer as OtelLayer};
+use opentelemetry::trace::TracerProvider;
+use opentelemetry_sdk::trace;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{prelude::*, EnvFilter};
+
+const DEFAULT_LOG_LEVEL: &str = "INFO";
+
+pub fn initialize_tracing(app_name: &'static str) -> trace::TracerProvider {
+    let formatting_layer = BunyanFormattingLayer::new(app_name.into(), std::io::stdout);
+    let tracer_provider = trace::TracerProvider::default();
+
+    // Set up link between OpenTelemetry and tracing crate
+    tracing_subscriber::registry()
+        .with(tracing_opentelemetry::OpenTelemetryLayer::new(
+            tracer_provider.tracer(app_name),
+        ))
+        .with(
+            EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(DEFAULT_LOG_LEVEL)),
+        )
+        .with(JsonStorageLayer)
+        .with(formatting_layer)
+        .init();
+
+    tracer_provider
+}
+
+pub fn otel_layer(
+    tracer_provider: &trace::TracerProvider,
+) -> OtelLayer<impl Fn() + Clone + use<'_>> {
+    OtelLayer::new(|| {
+        tracer_provider.force_flush();
+    })
+    // Set the "faas.trigger" attribute of the span to "pubsub"
+    .with_trigger(OpenTelemetryFaasTrigger::Http)
+}
