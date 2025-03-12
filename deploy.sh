328b1b060d50b51e18c9326b531eb73cfcfaf278
Nicolas Antinori <nicolas_antinori@hotmail.com>
2025-03-12 19:36:50 -0300

    fixed jwks

--- deploy.sh
+++ deploy.sh
@@ -2,24 +2,28 @@
 
 cargo lambda build --release
 
+ENV_REDIS_URL="redis://lambda-demo-permissions-x3duz1.serverless.sae1.cache.amazonaws.com:6379"
+ENV_JWKS_URL="https://hkpktejt2fjgm4to4ihskvuiha0uxxau.lambda-url.sa-east-1.on.aws/"
+LAMBDA_ROLE="arn:aws:iam::619071353885:role/lambda-execution-role"
+
 echo "deploying add_path lambda..."
 cargo lambda deploy add_path \
     --disable-function-url \
-    --role arn:aws:iam::619071353885:role/lambda-execution-role \
-    --env-var REDIS_URL="redis://lambda-demo-permissions-x3duz1.serverless.sae1.cache.amazonaws.com:6379"
+    --role $LAMBDA_ROLE \
+    --env-var REDIS_URL="$ENV_JWKS_URL"
 
 echo "deploying jwks lambda..."
 cargo lambda deploy jwks \
     --enable-function-url \
-    --role arn:aws:iam::619071353885:role/lambda-execution-role
+    --role $LAMBDA_ROLE
 
 echo "deploying validate_jwt lambda..."
 cargo lambda deploy validate_jwt \
     --role arn:aws:iam::619071353885:role/lambda-execution-role \
-    --env-var REDIS_URL="redis://lambda-demo-permissions-x3duz1.serverless.sae1.cache.amazonaws.com:6379" \
-    --env-var JWKS_URL="https://hkpktejt2fjgm4to4ihskvuiha0uxxau.lambda-url.sa-east-1.on.aws/"
+    --env-var REDIS_URL="$ENV_REDIS_URL" \
+    --env-var JWKS_URL="$ENV_JWKS_URL"
 
 echo "deploying echo lambda..."
 cargo lambda deploy echo \
-    --role arn:aws:iam::619071353885:role/lambda-execution-role
+    --role $LAMBDA_ROLE
 
--- tracing_handler/src/lib.rs
+++ tracing_handler/src/lib.rs
@@ -1,5 +1,3 @@
-use lambda_runtime::layers::{OpenTelemetryFaasTrigger, OpenTelemetryLayer as OtelLayer};
-use opentelemetry::trace::TracerProvider;
 use opentelemetry_sdk::trace;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_subscriber::{prelude::*, EnvFilter};
@@ -12,9 +10,6 @@ pub fn initialize_tracing(app_name: &'static str) -> trace::TracerProvider {
 
     // Set up link between OpenTelemetry and tracing crate
     tracing_subscriber::registry()
-        /*.with(tracing_opentelemetry::OpenTelemetryLayer::new(
-            tracer_provider.tracer(app_name),
-        ))*/
         .with(
             EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(DEFAULT_LOG_LEVEL)),
         )
@@ -24,14 +19,3 @@ pub fn initialize_tracing(app_name: &'static str) -> trace::TracerProvider {
 
     tracer_provider
 }
-
-/*
-pub fn otel_layer(
-    tracer_provider: &trace::TracerProvider,
-) -> OtelLayer<impl Fn() + Clone + use<'_>> {
-    OtelLayer::new(|| {
-        tracer_provider.force_flush();
-    })
-    // Set the "faas.trigger" attribute of the span to "pubsub"
-    .with_trigger(OpenTelemetryFaasTrigger::Http)
-}*/
