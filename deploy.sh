d37fa2296c4d904e41d84b313fd5c5d9614eddab
Nicolas Antinori <nicolas_antinori@hotmail.com>
2025-03-17 17:03:52 -0300

    added CargoLambda.toml

--- deploy.sh
+++ deploy.sh
@@ -1,40 +1,39 @@
 #!/bin/bash
-cargo lambda build --release
+cargo lambda build
 
 ENV_REDIS_URL="rediss://lambda-demo-permissions-x3duz1.serverless.sae1.cache.amazonaws.com:6379"
 ENV_JWKS_URL="https://hkpktejt2fjgm4to4ihskvuiha0uxxau.lambda-url.sa-east-1.on.aws/"
-LAMBDA_ROLE="arn:aws:iam::619071353885:role/lambda-execution-role"
 RUST_LOG="WARN"
 
 echo "deploying add_path lambda..."
 cargo lambda deploy add_path \
+    --context production \
     --disable-function-url \
-    --role $LAMBDA_ROLE \
     --env-var RUST_LOG="$RUST_LOG" \
     --env-var REDIS_URL="$ENV_REDIS_URL"
 
 echo "deploying remove_path lambda..."
 cargo lambda deploy remove_path \
+    --context production \
     --disable-function-url \
-    --role $LAMBDA_ROLE \
     --env-var RUST_LOG="$RUST_LOG" \
     --env-var REDIS_URL="$ENV_REDIS_URL"
 
 echo "deploying jwks lambda..."
 cargo lambda deploy jwks \
+    --context production \
     --enable-function-url \
-    --env-var RUST_LOG="$RUST_LOG" \
-    --role $LAMBDA_ROLE
+    --env-var RUST_LOG="$RUST_LOG"
 
 echo "deploying validate_jwt lambda..."
 cargo lambda deploy validate_jwt \
-    --role arn:aws:iam::619071353885:role/lambda-execution-role \
+    --context production \
     --env-var RUST_LOG="$RUST_LOG" \
     --env-var REDIS_URL="$ENV_REDIS_URL" \
     --env-var JWKS_URL="$ENV_JWKS_URL"
 
 echo "deploying echo lambda..."
 cargo lambda deploy echo \
-    --env-var RUST_LOG="$RUST_LOG" \
-    --role $LAMBDA_ROLE
+    --context production \
+    --env-var RUST_LOG="$RUST_LOG"
 
